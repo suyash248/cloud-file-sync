d5548e129d063a2719ba873918ea1556c9e85ba8
Suyash Soni <suyash.soni248@gmail.com>
2017-11-06 13:38:46 +0530

    Update settings.py
    updated settings

--- etc/conf/settings.py
+++ etc/conf/settings.py
@@ -1,7 +1,9 @@
 # S3 specific settings
 S3_ACCESSKEY = "your_s3_access_key"
 S3_SECRETKEY = "your_s3_secret_key"
-S3_BUCKET = "your_s3_bucket"
+
+# We assume that S3_BUCKET already exists.
+S3_BUCKET = "your_s3_bucket"  
 
 # In order to avoid duplicate uploads, we maintain journals of previously downloaded files along with their md5 hash
 LOCAL_JOURNAL_LOCATION = "~/.s3filesync.txt"
@@ -10,6 +12,7 @@ LOCAL_JOURNAL_LOCATION = "~/.s3filesync.txt"
 # delete it from local system.
 S3_FILE_NAMES_JOURNAL_LOCATION = "~/.s3filesync_filenames.txt"
 
+# NOTE: This LOCAL_SYNC_LOCATION directory must be created beforehand.
 # Location where remote files are downloaded, any modification in this directory(including sub directories) will trigger
 # upload process
 LOCAL_SYNC_LOCATION = '/Users/suyash/sync_loc'
