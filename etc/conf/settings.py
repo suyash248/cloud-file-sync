0348629fee262d13f0345b3ddd06247de70d3270
Suyash <suyash.soni248@gmail.com>
2017-11-06 14:26:40 +0530

    Creates local sync directory if not exists

--- etc/conf/settings.py
+++ etc/conf/settings.py
@@ -1,7 +1,7 @@
 # S3 specific settings
-S3_ACCESSKEY = "your_s3_access_key"
-S3_SECRETKEY = "your_s3_secret_key"
-S3_BUCKET = "your_s3_bucket"
+S3_ACCESSKEY = "AKIAIAX7NRWZBVWJQYTA"
+S3_SECRETKEY = "NTsKnU/sFIMy+5f71CbkYTvzX/OfyF9WYBiJqfAT"
+S3_BUCKET = "wigzoextra"
 
 # In order to avoid duplicate uploads, we maintain journals of previously downloaded files along with their md5 hash
 LOCAL_JOURNAL_LOCATION = "~/.s3filesync.txt"
@@ -12,4 +12,4 @@ S3_FILE_NAMES_JOURNAL_LOCATION = "~/.s3filesync_filenames.txt"
 
 # Location where remote files are downloaded, any modification in this directory(including sub directories) will trigger
 # upload process
-LOCAL_SYNC_LOCATION = '/Users/suyash/sync_loc'
+LOCAL_SYNC_LOCATION = '/home/wigzo/sync_loc'
--- tests/sync.py
+++ tests/sync.py
@@ -1,8 +1,17 @@
 from threading import Thread
+import os
 from core.download import Downloader
 from lib.watcher import watch
+from etc.conf import settings
+
+def create_local_sync_dir():
+    import os
+    if not os.path.exists(settings.LOCAL_SYNC_LOCATION):
+        print "[SYNC_LOC] Going to create local sync directory %s" %settings.LOCAL_SYNC_LOCATION
+        os.makedirs(settings.LOCAL_SYNC_LOCATION)
 
 def sync():
+    create_local_sync_dir()
     download_sync_thread = Thread(target=Downloader.download_and_sync)
     download_sync_thread.start()
     download_sync_thread.join()
